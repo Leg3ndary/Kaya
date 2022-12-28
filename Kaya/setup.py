57f37868c151a3585c6dab14256aa45ad2d34661
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-12-27 22:24:25 -0500

    Add some requirements and fun setup script

---
+++ Kaya/setup.py
@@ -0,0 +1,32 @@
+from cx_Freeze import Executable, setup
+import sys
+
+sys.setrecursionlimit(1500)
+
+base = None # "Win32GUI"
+
+executables = [Executable("main.py", base=base)]
+
+packages = [
+    "speech_recognition",
+    "pyttsx3",
+    "httpx",
+    "datetime",
+    "json",
+    "typing",
+    "os",
+    "sys",
+]
+options = {
+    "build_exe": {
+        "packages": packages,
+    },
+}
+
+setup(
+    name="Kaya",
+    options=options,
+    version="0.1.0",
+    description="Kaya PA",
+    executables=executables,
+)
